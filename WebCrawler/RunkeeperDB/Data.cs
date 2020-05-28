cec37e14857376b55918bb36afa1f74fc9bc27aa
Dawid Mroczek <dawid.mroczek@altium.com>
2020-05-28 16:13:48 +0200

    Data classes added

---
+++ WebCrawler/RunkeeperDB/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler.RunkeeperDB
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string ActivityId { get; set; }
+        public float Distance { get; set; }
+        public DateTime ActivityDateTime { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Username { get; set; }
+        public DateTime ElapsedTime { get; set; }
+    }
+}
---
+++ WebCrawler/RunkeeperDB/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler.RunkeeperDB
+{
+    public class DataContext: DbContext
+    {
+        DbSet<>
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connString = @"Data Source=daw.database.windows.net;Initial Catalog=Runkeeper;User ID=dawid;Password=At@kujaAZ8;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            options.UseSqlServer(connString);
+        }
+    }
+}
